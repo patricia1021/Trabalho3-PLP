90e3698777e203ee47f726ab74b6630147e43930
Patrícia Guimarães <deud.patricia@yahoo.com.br>
2019-07-03 11:12:50 -0300

    Começo das classes Banco e Conta

---
+++ Banco.java
@@ -0,0 +1,4 @@
+public class Banco
+{
+    ArrayList<Conta> contas = new ArrayList();
+}
---
+++ Conta.java
@@ -0,0 +1,5 @@
+public class Conta
+{
+    private String nome = new String();
+    private double saldo = 10000;
+}
