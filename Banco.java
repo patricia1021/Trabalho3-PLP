d9a7b48e65a0baf5b60c02620efe8efd4136e929
Patrícia Guimarães <deud.patricia@yahoo.com.br>
2019-07-04 14:02:12 -0300

    Update Banco.java

--- Banco.java
+++ Banco.java
@@ -2,8 +2,15 @@ import java.util.ArrayList;
 
 public class Banco
 {
-    ArrayList<Conta> contas = new ArrayList();
+    double const val_inicio =10000;
+    ArrayList<Conta> contas = new ArrayList(5);
     
+    contas.add("Bruno";val_inicio);
+    contas.add("Kalyl";val_inicio);
+    contas.add("Marcus";val_inicio);
+    contas.add("Luciana";val_inicio);
+    contas.add("Patricia";val_inicio);
+        
     public void transferencia(Conta de,Conta para,double valor)
     {
         wait();
