4d1f9e0d88893df7e1a0ad10117d1f4ab5f107da
brunosaales <brunocesar.bsi@gmail.com>
2019-07-09 18:11:30 -0300

    Update da classe

--- TransferThread.java
+++ TransferThread.java
@@ -5,6 +5,7 @@ class TransferThread extends Thread
 {
   private Banco banco;
   private Conta contaorigem;
+  private int de;
   
   public TransferThread (Banco b, int de) 
   {
@@ -12,7 +13,7 @@ class TransferThread extends Thread
     contaorigem = (Conta) b.getContas().get(de);
   }
 
-  public void run ( )
+  public void run ()
   {
     try
     {
@@ -21,15 +22,14 @@ class TransferThread extends Thread
         
         do{
             de = (int)((Math.random()*100)%5);
-        } while(de < 0 | de > 4);
-
+        } while(de < 0 | de > 4 | banco.getConta(de).getNome().equals(contaorigem.getNome()));
+        //validação para verificar se a conta de destino não é a mesma que a conta origem
         double valor = ((Math.random()*100));
 
         Conta contadestino = (Conta) banco.getContas().get(de);
-        System.out.println("Transferencia de "+ valor  + " conta origem: "+ contaorigem.getNome() 
-        +" conta destino: " + contadestino.getNome());
-        
-        banco.transferencia(contaorigem, contadestino, valor);
+        System.out.println(contaorigem.getNome() + " transferiu R$"+ String.format("%.2f", valor) 
+        +" para a conta de " + contadestino.getNome());
+          banco.transferencia(contaorigem, contadestino, valor);
         // define o valor a ser transferido aleatoriamente
         // faz a operação de transferência
         sleep(1000);
